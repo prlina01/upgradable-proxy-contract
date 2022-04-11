effed251b64802e9260103dd8e2e02f02a82b4a4
prlina01 <36077702+prlina01@users.noreply.github.com>
2022-04-11 11:22:35 +0200

    Add fallback function to the proxy contract that gets call data and passes it to the implementation contract.

--- contracts/Proxy.sol
+++ contracts/Proxy.sol
@@ -11,4 +11,31 @@ contract Proxy {
         return implementation;
     }
 
+    fallback() external {
+        assembly {
+            let ptr := mload(0x40)
+            calldatacopy(ptr, 0, calldatasize())
+
+            let result := delegatecall(
+            gas(),
+            sload(implementation.slot),
+            ptr,
+            calldatasize(),
+            0,
+            0
+            )
+
+            let size := returndatasize()
+            returndatacopy(ptr, 0, size)
+
+            switch result
+            case 0 {
+                revert(ptr, size)
+            }
+            default {
+                return(ptr, size)
+            }
+        }
+    }
+
 }
