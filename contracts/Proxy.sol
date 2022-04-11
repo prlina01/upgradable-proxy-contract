1e70b50bf584b211ac0cb98e38841201496a6dc6
prlina01 <36077702+prlina01@users.noreply.github.com>
2022-04-11 12:49:34 +0200

    Implementation slot from callee (Proxy) contract  will no longer be 0 and overlap with the magicNumber slot from called (Logic) contract.

--- contracts/Proxy.sol
+++ contracts/Proxy.sol
@@ -1,29 +1,39 @@
 pragma solidity ^0.8.4;
 
+library StorageSlot {
+    function getAddressAt(bytes32 slot) internal view returns (address a) {
+        assembly {
+            a := sload(slot)
+        }
+    }
+
+    function setAddressAt(bytes32 slot, address address_) internal {
+        assembly {
+            sstore(slot, address_)
+        }
+    }
+}
+
+
 contract Proxy {
-    address public implementation;
+
+    bytes32 private constant _IMPL_SLOT =
+    bytes32(uint256(keccak256("eip1967.proxy.implementation")) - 1);
 
     function setImplementation(address implementation_) public {
-        implementation = implementation_;
+        StorageSlot.setAddressAt(_IMPL_SLOT, implementation_);
     }
 
     function getImplementation() public view returns (address) {
-        return implementation;
+        return StorageSlot.getAddressAt(_IMPL_SLOT);
     }
 
-    fallback() external {
+    function _delegate(address impl) internal virtual {
         assembly {
             let ptr := mload(0x40)
             calldatacopy(ptr, 0, calldatasize())
 
-            let result := delegatecall(
-            gas(),
-            sload(implementation.slot),
-            ptr,
-            calldatasize(),
-            0,
-            0
-            )
+            let result := delegatecall(gas(), impl, ptr, calldatasize(), 0, 0)
 
             let size := returndatasize()
             returndatacopy(ptr, 0, size)
@@ -36,6 +46,11 @@ contract Proxy {
                 return(ptr, size)
             }
         }
+
+    }
+
+    fallback() external {
+        _delegate(StorageSlot.getAddressAt(_IMPL_SLOT));
     }
 
 }
